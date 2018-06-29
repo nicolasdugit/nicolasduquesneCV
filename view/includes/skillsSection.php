4e13315b24f1fbb5818cfc133a217fdfb368bd3c
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-29 17:12:04 +0700

    redaction contenu skillsSection

--- view/includes/skillsSection.php
+++ view/includes/skillsSection.php
@@ -12,9 +12,9 @@
 				<h4><b>DEVELOPPEMENT WEB</b></h4>
 				<h6 class="expert">CONFIRMÉ</h6>
 				
-				<p class="margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus aliquam neque sit amet sodales. 
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que velit, 
-					quis consequat nulla effi citur at.</p>
+				<p class="margin-tb-30">La connaissance des langages HTML, CSS, JS et PHP me permet de répondre à vos besoins en terme de création de site web. <br>
+					Et comme le web évolue constamment, je continue de me perfectionnr sur ces langages et à en apprendre des nouveaux.
+				</p>
 					
 				<div class="line-progress margin-b-20" data-prog-percent=".90"><div></div>
 					<p class="progress-title"><i class="ion-social-html5"></i><b> HTML</b></p></div>
@@ -33,9 +33,9 @@
 				<h4><b>FRAMEWORK ET CMS</b></h4>
 				<h6 class="expert">CONFIRMÉ</h6>
 				
-				<p class="margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus aliquam neque sit amet sodales. 
-					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que velit, 
-					quis consequat nulla effi citur at.</p>
+				<p class="margin-tb-30">De nombreux outils sont mis à la dispositions des développeurs web pour leur permettre de travailler plus efficacement. <br>
+					Ma formation et mes expériences personnels me poussent à en apprendre davantage chaques jours. <br>
+				</p>
 					
 				<div class="line-progress margin-b-20" data-prog-percent=".90"><div></div>
 					<p class="progress-title"><i class="ion-social-wordpress"></i><b> WORDPRESS</b></p></div>
