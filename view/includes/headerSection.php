defca15facf082290e87e61ed772c9328cebab64
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-29 11:05:11 +0700

    include footer header intro dans template

---
+++ view/includes/headerSection.php
@@ -0,0 +1,39 @@
+<header>
+	<div class="container">
+		<div class="heading-wrapper">
+			<div class="row">
+				<div class="col-sm-6 col-md-6 col-lg-4">
+					<div class="info">
+						<i class="icon ion-ios-location-outline"></i>
+						<div class="right-area">
+							<h5>3008 Sarah Drive</h5>
+							<h5>Franklin,LA 70538</h5>
+						</div><!-- right-area -->
+					</div><!-- info -->
+				</div><!-- col-sm-4 -->
+				
+				<div class="col-sm-6 col-md-6 col-lg-4">
+					<div class="info">
+						<i class="icon ion-ios-telephone-outline"></i>
+						<div class="right-area">
+							<h5>337-4139538</h5>
+							<h6>MIN - FRI,8AM - 7PM</h6>
+						</div><!-- right-area -->
+					</div><!-- info -->
+				</div><!-- col-sm-4 -->
+				
+				<div class="col-sm-6 col-md-6 col-lg-4">
+					<div class="info">
+						<i class="icon ion-ios-chatboxes-outline"></i>
+						<div class="right-area">
+							<h5>contact@colorlib.com</h5>
+							<h6>REPLY IN 24 HOURS</h6>
+						</div><!-- right-area -->
+					</div><!-- info -->
+				</div><!-- col-sm-4 -->
+			</div><!-- row -->
+		</div><!-- heading-wrapper -->
+		
+		<a class="downlad-btn" href="#">Download CV</a>
+	</div><!-- container -->
+</header>
---
+++ view/includes/introSection.php
@@ -0,0 +1,26 @@
+<section class="intro-section">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-1 col-lg-2"></div>
+			<div class="col-md-10 col-lg-8">
+				<div class="intro">
+					<div class="profile-img"><img src="images/profile-1-250x250.jpg" alt=""></div>
+					<h2><b>Michel SMith</b></h2>
+					<h4 class="font-yellow">Key Account Manager</h4>
+					<ul class="information margin-tb-30">
+						<li><b>BORN : </b>August 25, 1987</li>
+						<li><b>EMAIL : </b>mymith@mywebpage.com</li>
+						<li><b>MARITAL STATUS : </b>Married</li>
+					</ul>
+					<ul class="social-icons">
+						<li><a href="#"><i class="ion-social-pinterest"></i></a></li>
+						<li><a href="#"><i class="ion-social-linkedin"></i></a></li>
+						<li><a href="#"><i class="ion-social-instagram"></i></a></li>
+						<li><a href="#"><i class="ion-social-facebook"></i></a></li>
+						<li><a href="#"><i class="ion-social-twitter"></i></a></li>
+					</ul>
+				</div><!-- intro -->
+			</div><!-- col-sm-8 -->
+		</div><!-- row -->
+	</div><!-- container -->
+</section><!-- intro-section -->
