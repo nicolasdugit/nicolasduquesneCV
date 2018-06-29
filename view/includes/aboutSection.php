e4d51902df64431eca71707ad1f5b404a34054b3
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-29 17:19:17 +0700

    modification alignement text aboutSection

--- view/includes/aboutSection.php
+++ view/includes/aboutSection.php
@@ -8,7 +8,7 @@
 				</div>
 			</div><!-- col-sm-3 -->
 			<div class="col-sm-12 col-md-12">
-				<p class="margin-b-30">Après 10 ans d’expérience professionnelle en tant que Dessinateur industriel, je profite d’un déménagement à l’étranger pour sauter le pas et entreprendre une nouvelle carrière dans un domaine qui me passionne depuis de nombreuses années: le développement web. <br>
+				<p class="margin-b-30 text-center">Après 10 ans d’expérience professionnelle en tant que Dessinateur industriel, je profite d’un déménagement à l’étranger pour sauter le pas et entreprendre une nouvelle carrière dans un domaine qui me passionne depuis de nombreuses années: le développement web. <br>
 				C’est dans la ville de Vientiane, capitale du Laos, où je vis actuellement, que je m’initie en autodidacte à cette nouvelle activité, en réalisant plusieurs sites internet pour des proches. <br>
 				Puis je me professionnalise en intégrant le cursus diplômant en développement web d’Openclassroooms, structure renommée et reconnue par l’État. <br>
 				Je maîtrise à présent les langages (HTML, CSS, JavaScript et PHP) et outils (wordpress, bootstrap, github) nécessaires au développement de sites webs adaptables à tous types de support. J’apporte également une grande attention au respect des standard du web et des normes en vigueurs, et à l’amélioration du référencement naturel. <br>
