7df9f7f89176ad2bab39032565aaaa90abe12c78
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-29 17:56:41 +0700

    modification contenu experienceSection

--- view/includes/experienceSection.php
+++ view/includes/experienceSection.php
@@ -9,17 +9,14 @@
 			</div>
 			<div class="col-sm-12">
 				<div class="experience margin-b-50">
-					<h4><b>DEVELOPPEUR WEB AUTO ENTREPRENEUR</b></h4>
+					<h4><b>DEVELOPPEUR WEB INDEPENDANT</b></h4>
 					<h5 class="font-yellow"><b>UPSTAIRSLAB</b></h5>
 					<h6 class="font-lite-black margin-t-10">SEPTEMBRE 2017 - AUJOURD'HUI</h6>
-					<p class="margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus aliquam neque sit amet sodales. 
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que velit, 
-						quis consequat nulla effi citur at. Maecenas sed massa tristique.Duis non volutpat arcu, 
-						eu mollis tellus. Sed finibus aliquam neque sit amet sodales. </p>
+					<p class="margin-tb-30"> Depuis septembre 2017, je travaille comme developpeur independant au sein de l'équipe <a href="http://www.upstairslab.nicolasduquesne">d'UpstairsLab</a></p>
 					<ul class="list margin-b-30">
-						<li><span>Duis non volutpat arcu, eu mollis tellus.</span></li>
-						<li><span>Quis consequat nulla effi citur at.</span></li>
-						<li><span>Sed finibus aliquam neque sit.</span></li>
+						<li><span>Développement de site web</span></li>
+						<li><span>Organisation de Vientiane Tech Talks : conférences autour des nouvelles technologies</span></li>
+						<li><span>Formation diplomante sur <a href="http://www.openclassrooms.fr">Openclassrooms</a></span></li>
 					</ul>
 				</div>
 			</div>
