77aaa883fa76e10c372c46f967ca08338bb5b197
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-29 16:59:44 +0700

    redaction contenu aboutSection

--- view/includes/skillsSection.php
+++ view/includes/skillsSection.php
@@ -3,40 +3,49 @@
 		<div class="row">
 			<div class="col-sm-12 center-text">
 				<div class="heading">
-					<h3><b>Technical Skills</b></h3>
-					<h6 class="font-lite-black"><b>I'M GOOD AT</b></h6>
+					<h3><b>Compétences Techniques</b></h3>
+					<h6 class="font-lite-black"><b>MES SPÉCIALITÉS</b></h6>
 				</div>
-			</div><!-- col-sm-12 -->
+			</div>
 		
 			<div class="col-sm-12 col-md-6">
-				<h4><b>HTML & CSS</b></h4>
-				<h6 class="expert">EXPERT</h6>
+				<h4><b>DEVELOPPEMENT WEB</b></h4>
+				<h6 class="expert">CONFIRMÉ</h6>
 				
 				<p class="margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus aliquam neque sit amet sodales. 
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que velit, 
 					quis consequat nulla effi citur at.</p>
 					
 				<div class="line-progress margin-b-20" data-prog-percent=".90"><div></div>
-					<p class="progress-title"><b>HTML</b></p></div>
+					<p class="progress-title"><i class="ion-social-html5"></i><b> HTML</b></p></div>
 				
-				<div class="line-progress margin-b-50" data-prog-percent=".85"><div></div>
-					<p class="progress-title"><b>CSS</b></p></div>
-			</div><!-- col-md-6 -->
+				<div class="line-progress margin-b-20" data-prog-percent=".85"><div></div>
+					<p class="progress-title"><i class="ion-social-css3"></i><b> CSS</b></p></div>
+
+				<div class="line-progress margin-b-20" data-prog-percent=".70"><div></div>
+					<p class="progress-title"><i class="ion-social-javascript"></i><b> JavaScript</b></p></div>
+
+				<div class="line-progress margin-b-50" data-prog-percent=".70"><div></div>
+					<p class="progress-title"><i class="ion-social-php"></i><b> PHP</b></p></div>
+			</div>
 			
 			<div class="col-sm-12 col-md-6">
-				<h4><b>GRAPHIC DESIGN</b></h4>
-				<h6 class="expert">EXPERT</h6>
+				<h4><b>FRAMEWORK ET CMS</b></h4>
+				<h6 class="expert">CONFIRMÉ</h6>
 				
 				<p class="margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus aliquam neque sit amet sodales. 
 					Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que velit, 
 					quis consequat nulla effi citur at.</p>
 					
-				<div class="line-progress margin-b-20" data-prog-percent=".97"><div></div>
-					<p class="progress-title"><b>PHOTOSHOP</b></p></div>
+				<div class="line-progress margin-b-20" data-prog-percent=".90"><div></div>
+					<p class="progress-title"><i class="ion-social-wordpress"></i><b> WORDPRESS</b></p></div>
 				
-				<div class="line-progress margin-b-50" data-prog-percent=".97"><div></div>
-					<p class="progress-title"><b>ILLUSTRATOR</b></p></div>
-			</div><!-- col-md-6 -->
-		</div><!-- row -->
-	</div><!-- container -->
-</section><!-- skills-section -->
+				<div class="line-progress margin-b-20" data-prog-percent=".90"><div></div>
+					<p class="progress-title"><b> BOOTSTRAP</b></p></div>
+
+				<div class="line-progress margin-b-50" data-prog-percent=".60"><div></div>
+					<p class="progress-title"><b> HUGO</b></p></div>
+			</div>
+		</div>
+	</div>
+</section>
