c323a8466c2621d97de4ba4d49875056771c2ea2
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-29 16:13:25 +0700

    redaction contenu experienceSection

--- view/includes/experienceSection.php
+++ view/includes/experienceSection.php
@@ -3,16 +3,15 @@
 		<div class="row">
 			<div class="col-sm-12">
 				<div class="heading">
-					<h3><b>Work Experience</b></h3>
-					<h6 class="font-lite-black"><b>PREVIOUS JOBS</b></h6>
+					<h3><b>Expériences Profesionnelles</b></h3>
+					<h6 class="font-lite-black"><b></b></h6>
 				</div>
-			</div><!-- col-sm-3 -->
-			
+			</div>
 			<div class="col-sm-12">
 				<div class="experience margin-b-50">
-					<h4><b>JUNIOR PROJECT MANAGER</b></h4>
-					<h5 class="font-yellow"><b>DESIGN STUDIO</b></h5>
-					<h6 class="font-lite-black margin-t-10">MARCH 2015 - PRESENT</h6>
+					<h4><b>DEVELOPPEUR WEB AUTO ENTREPRENEUR</b></h4>
+					<h5 class="font-yellow"><b>UPSTAIRSLAB</b></h5>
+					<h6 class="font-lite-black margin-t-10">SEPTEMBRE 2017 - AUJOURD'HUI</h6>
 					<p class="margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus aliquam neque sit amet sodales. 
 						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que velit, 
 						quis consequat nulla effi citur at. Maecenas sed massa tristique.Duis non volutpat arcu, 
@@ -22,24 +21,8 @@
 						<li><span>Quis consequat nulla effi citur at.</span></li>
 						<li><span>Sed finibus aliquam neque sit.</span></li>
 					</ul>
-				</div><!-- experience -->
-				
-				<div class="experience margin-b-50">
-					<h4><b>WEB MASTER/WEB DEVELOPER</b></h4>
-					<h5 class="font-yellow"><b>DESIGN & WEB STUDIO</b></h5>
-					<h6 class="font-lite-black margin-t-10">APRIL 2014 - FEBRUARY 2015</h6>
-					<p class="margin-tb-30">Duis non volutpat arcu, eu mollis tellus. Sed finibus aliquam neque sit amet sodales. 
-						Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla maximus pellentes que velit, 
-						quis consequat nulla effi citur at. Maecenas sed massa tristique.Duis non volutpat arcu, 
-						eu mollis tellus. Sed finibus aliquam neque sit amet sodales. </p>
-					<ul class="list">
-						<li><span>Duis non volutpat arcu, eu mollis tellus.</span></li>
-						<li><span>Quis consequat nulla effi citur at.</span></li>
-						<li><span>Sed finibus aliquam neque sit.</span></li>
-					</ul>
-				</div><!-- experience -->
-				
-			</div><!-- col-sm-9 -->
-		</div><!-- row -->
-	</div><!-- container -->
-</section><!-- experience-section -->
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
