938e1375320c089dc80f43e25dafa21d8513ffca
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-29 18:25:01 +0700

    modification contenu portfolioSection

--- view/includes/portfolioSection.php
+++ view/includes/portfolioSection.php
@@ -4,60 +4,60 @@
 			<div class="col-sm-12">
 				<div class="heading">
 					<h3><b>Portfolio</b></h3>
-					<h6 class="font-lite-black"><b>MES TRAVAUX</b></h6>
+					<h6 class="font-lite-black"><b>MES TRAVAUX RÉALISÉS</b></h6>
 				</div>
 			</div>
 			
 			<div class="col-sm-12">
 				<div class="portfolioFilter clearfix margin-b-80">
 					<a href="#" data-filter="*" class="current"><b>TOUS</b></a>
-					<a href="#" data-filter=".web-design"><b>WEB DESIGN</b></a>
-					<a href="#" data-filter=".branding"><b>BRANDING</b></a>
-					<a href="#" data-filter=".graphic-design"><b>GRAPHIC DESIGN</b></a>
+					<a href="#" data-filter=".pro"><b>PRO</b></a>
+					<a href="#" data-filter=".studies"><b>ETUDES</b></a>
+					<a href="#" data-filter=".perso"><b>PERSO</b></a>
 				</div>
 			</div>
 		</div>
 		
 		<div class="portfolioContainer margin-b-50">
 			
-			<div class="p-item web-design">
+			<div class="p-item pro">
 				<a href="public/images/portfolio-13-400x400.jpg" data-fluidbox>
-					<img src="public/images/portfolio-13-400x400.jpg" alt=""></a>
+					<img src="public/images/portfolio-13-400x400.jpg" alt="Poissonnerie du Port Chez Elise"></a>
 			</div><!-- p-item -->
 			
-			<div class="p-item branding graphic-design">
+			<div class="p-item studies">
 				<a href="public/images/portfolio-14-400x400.jpg" data-fluidbox>
-					<img src="public/images/portfolio-14-400x400.jpg" alt=""></a>
+					<img src="public/images/portfolio-14-400x400.jpg" alt="Web Agency"></a>
 			</div><!-- p-item -->
 			
-			<div class="p-item web-design">
+			<div class="p-item pro">
 				<a href="public/images/portfolio-15-400x400.jpg" data-fluidbox>
-					<img src="public/images/portfolio-15-400x400.jpg" alt=""></a>
+					<img src="public/images/portfolio-15-400x400.jpg" alt="Perlouse Pigalle"></a>
 			</div><!-- p-item -->
 			
-			<div class="p-item graphic-design">
+			<div class="p-item studies">
 				<a class="img" href="public/images/portfolio-16-400x400.jpg" data-fluidbox>
-					<img src="public/images/portfolio-16-400x400.jpg" alt=""></a>
+					<img src="public/images/portfolio-16-400x400.jpg" alt="Strasbourg Tourisme"></a>
 			</div><!-- p-item -->
 			
-			<div class="p-item branding graphic-design">
+			<div class="p-item pro">
 				<a href="public/images/portfolio-17-400x400.jpg" data-fluidbox>
-					<img src="public/images/portfolio-17-400x400.jpg" alt=""></a>
+					<img src="public/images/portfolio-17-400x400.jpg" alt="UpstairsLab"></a>
 			</div><!-- p-item -->
 			
-			<div class="p-item graphic-design web-design">
+			<div class="p-item studies">
 				<a href="public/images/portfolio-18-400x400.jpg" data-fluidbox>
-					<img src="public/images/portfolio-18-400x400.jpg" alt=""></a>
+					<img src="public/images/portfolio-18-400x400.jpg" alt="ResaVelov Lyon"></a>
 			</div><!-- p-item -->
 			
-			<div class="p-item  graphic-design branding">
+			<div class="p-item studies">
 				<a href="public/images/portfolio-19-400x400.jpg" data-fluidbox>
-					<img src="public/images/portfolio-19-400x400.jpg" alt=""></a>
+					<img src="public/images/portfolio-19-400x400.jpg" alt="Blog Ecrivain"></a>
 			</div><!-- p-item -->
 				
-			<div class="p-item web-design branding">
+			<div class="p-item perso">
 				<a href="public/images/portfolio-20-400x400.jpg" data-fluidbox>
-					<img src="public/images/portfolio-20-400x400.jpg" alt=""></a>
+					<img src="public/images/portfolio-20-400x400.jpg" alt="Snake"></a>
 			</div><!-- p-item -->
 		
 		</div><!-- portfolioContainer -->
