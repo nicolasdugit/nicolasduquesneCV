7b3bee9f81f62ea6f66bb429643581daede67870
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-29 17:02:02 +0700

    redaction contenu progressSection

--- view/includes/progressSection.php
+++ view/includes/progressSection.php
@@ -3,16 +3,16 @@
 		<div class="row">
 			<div class="col-sm-6 col-md-6 col-lg-3">
 				<div class="radial-prog-area margin-b-30">
-					<div class="radial-progress" data-prog-percent=".97">
+					<div class="radial-progress" data-prog-percent=".90">
 						<div></div>
-						<h6 class="progress-title">HTML5 & CSS3</h6>
+						<h6 class="progress-title">DEVELOPPEMENT WEB</h6>
 					</div>
 				</div><!-- radial-prog-area-->
 			</div><!-- col-sm-6-->
 		
 			<div class="col-sm-6 col-md-6 col-lg-3">
 				<div class="radial-prog-area margin-b-30">
-					<div class="radial-progress" data-prog-percent=".78">
+					<div class="radial-progress" data-prog-percent=".70">
 						<div></div>
 						<h6 class="progress-title">WEB DESIGN</h6>
 					</div>
@@ -21,18 +21,18 @@
 			
 			<div class="col-sm-6 col-md-6 col-lg-3">
 				<div class="radial-prog-area margin-b-30">
-					<div class="radial-progress" data-prog-percent=".67">
+					<div class="radial-progress" data-prog-percent=".90">
 						<div></div>
-						<h6 class="progress-title">JAVA</h6>
+						<h6 class="progress-title">RESPONSIVE DESIGN</h6>
 					</div>
 				</div><!-- radial-prog-area-->
 			</div><!-- col-sm-6-->
 			
 			<div class="col-sm-6 col-md-6 col-lg-3">
 				<div class="radial-prog-area margin-b-50">
-					<div class="radial-progress" data-prog-percent=".97">
+					<div class="radial-progress" data-prog-percent=".80">
 						<div></div>
-						<h6 class="progress-title">PHP</h6>
+						<h6 class="progress-title">REFERENCEMENT</h6>
 					</div>
 				</div><!-- radial-prog-area-->
 			</div><!-- col-sm-6-->
