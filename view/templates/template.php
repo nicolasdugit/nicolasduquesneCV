ecc28186a525bc645f5d075986c8da5cf1dcd0cd
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-30 12:01:13 +0700

    ajour des balise meta dans le head

--- view/templates/template.php
+++ view/templates/template.php
@@ -6,6 +6,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta charset="UTF-8">
 
+	<meta name="description" content="<?= $description ?>" />
+	<meta name="keywords" content="<?= $keywords ?>" />
+	<meta name="author" content="Nicolas Duquesne" />
+
+	<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content="<?= $title ?>"/>
+	<meta property="og:image" content="public/images/logo80px.png"/>
+	<meta property="og:url" content="http://www.nicolasduquesne.com"/>
+	<meta property="og:site_name" content="Nicolas Duquesne Développeur Web"/>
+	<meta property="og:description" content="<?= $description ?>"/>
+	<meta name="twitter:title" content="<?= $title ?>" />
+	<meta name="twitter:image" content="public/images/llogo80px.png" />
+	<meta name="twitter:url" content="http://www.nicolasduquesne.com" />
+	<meta name="twitter:card" content="" />
+
 	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
 	<link rel="shortcut icon" href="public/images/logo80px.png">
 	
