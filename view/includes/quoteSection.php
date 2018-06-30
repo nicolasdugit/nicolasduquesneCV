58ec3f486c019a87002cfe3178e2f8b8d2538967
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-06-30 14:22:32 +0700

    ajout des liens vers collaborateurs en h3

--- view/includes/quoteSection.php
+++ view/includes/quoteSection.php
@@ -6,14 +6,13 @@
 				<div class="slider">
 				<h3 class="font-yellow"><b>Collaborateurs</b></h3>
 				<i class="icon ion-quote"></i>
+				<hr>
 					<figure>
-						<div><a href="">GEOQUANTS</a></div>
-						<div><a href="">RESEARCH DATA BOT</a></div>
-						<div><a href="">UPSTAIRSLAB</a></div>
-						<div><a href="">GEOQUANTS</a></div>
-						
+						<div><h3><a href="https://www.geoquants.com/">GEOQUANTS</a></h3></div>
+						<div><h3><a href="https://researchdatarobot.com/">RESEARCH DATA BOT</a></h3></div>
+						<div><h3><a href="http://www.upstairslab.nicolasduquesne.com">UPSTAIRSLAB</a></h3></div>
+						<div><h3><a href="https://www.geoquants.com/">GEOQUANTS</a></h3></div>
 					</figure>
-					
     			</div>
 			</div>
 		</div>
