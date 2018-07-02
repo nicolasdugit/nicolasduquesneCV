99196888d7dd146003fc9a3fa2c4b187c2e02502
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-07-02 16:20:16 +0700

    modification bouton et lein vers site dans modal

--- view/fr/includes/modal.php
+++ view/fr/includes/modal.php
@@ -12,11 +12,12 @@
 					<div class="col-lg-8 mx-auto">
 						<div class="modal-body">
 						<!-- Project Details Go Here -->
-							<a href="http://www.poissonnerieduport.fr" target="_blank">
-								<h2 class="text-uppercase">Poissonnerie du Port Chez Elise</h2>
-							</a>
+							<h2 class="text-uppercase">Poissonnerie du Port Chez Elise</h2>
 							<p class="item-intro text-muted">Site Web pour une poissonnerie</p>
-						<button class="btn btn-warning" data-dismiss="modal" type="button"><i class="fa fa-times"></i> Fermer</button>
+							<div class="row">
+								<a class="col-md-3 offset-md-2 btn btn-success" href="http://www.poissonnerieduport.fr" target="_blank">Voir le site</a>
+								<button class="col-md-3 offset-md-2 btn btn-warning" data-dismiss="modal" type="button"><i class="fa fa-times"></i> Fermer</button>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -37,11 +38,12 @@
 					<div class="col-lg-8 mx-auto">
 						<div class="modal-body">
 						<!-- Project Details Go Here -->
-							<a href="http://www.webagency.nicolasduquesne.com" target="_blank">
-								<h2 class="text-uppercase">Web Agency</h2>
-							</a>
+							<h2 class="text-uppercase">Web Agency</h2>
 							<p class="item-intro text-muted">L'Agence de tous vos projets</p>
-						<button class="btn btn-warning" data-dismiss="modal" type="button"><i class="fa fa-times"></i> Fermer</button>
+							<div class="row">
+								<a class="col-md-3 offset-md-2 btn btn-success" href="http://www.webagency.nicolasduquesne.com" target="_blank">Voir le site</a>
+								<button class="col-md-3 offset-md-2 btn btn-warning" data-dismiss="modal" type="button"><i class="fa fa-times"></i> Fermer</button>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -62,11 +64,12 @@
 					<div class="col-lg-8 mx-auto">
 						<div class="modal-body">
 						<!-- Project Details Go Here -->
-							<a href="http://www.perlousepigalle.fr" target="_blank">
-								<h2 class="text-uppercase">Perlouse Pigale</h2>
-							</a>
+							<h2 class="text-uppercase">Perlouse Pigale</h2>
 							<p class="item-intro text-muted">Bar à huitres</p>
-						<button class="btn btn-warning" data-dismiss="modal" type="button"><i class="fa fa-times"></i> Fermer</button>
+							<div class="row">
+								<a class="col-md-3 offset-md-2 btn btn-success" href="http://www.perlousepigalle.fr" target="_blank">Voir le site</a>
+								<button class="col-md-3 offset-md-2 btn btn-warning" data-dismiss="modal" type="button"><i class="fa fa-times"></i> Fermer</button>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -87,11 +90,12 @@
 					<div class="col-lg-8 mx-auto">
 						<div class="modal-body">
 						<!-- Project Details Go Here -->
-							<a href="http://www.strasbourg-tourisme.nicolasduquesne.com" target="_blank">
-								<h2 class="text-uppercase">Office de tourisme de Strasbourg</h2>
-							</a>
+							<h2 class="text-uppercase">Office de tourisme de Strasbourg</h2>
 							<p class="item-intro text-muted">Ville de Strasbourg</p>
-						<button class="btn btn-warning" data-dismiss="modal" type="button"><i class="fa fa-times"></i> Fermer</button>
+							<div class="row">
+								<a class="col-md-3 offset-md-2 btn btn-success" href="http://www.strasbourg-tourisme.nicolasduquesne.com" target="_blank">Voir le site</a>
+								<button class="col-md-3 offset-md-2 btn btn-warning" data-dismiss="modal" type="button"><i class="fa fa-times"></i> Fermer</button>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -112,11 +116,12 @@
 					<div class="col-lg-8 mx-auto">
 						<div class="modal-body">
 						<!-- Project Details Go Here -->
-							<a href="http://www.upstairslab.nicolasduquesne.com" target="_blank">
-								<h2 class="text-uppercase">UpstairsLab</h2>
-							</a>
+							<h2 class="text-uppercase">UpstairsLab</h2>
 							<p class="item-intro text-muted">Event about tech</p>
-						<button class="btn btn-warning" data-dismiss="modal" type="button"><i class="fa fa-times"></i> Fermer</button>
+							<div class="row">
+								<a class="col-md-3 offset-md-2 btn btn-success" href="http://www.upstairslab.nicolasduquesne.com" target="_blank">Voir le site</a>
+								<button class="col-md-3 offset-md-2 btn btn-warning" data-dismiss="modal" type="button"><i class="fa fa-times"></i> Fermer</button>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -137,11 +142,12 @@
 					<div class="col-lg-8 mx-auto">
 						<div class="modal-body">
 						<!-- Project Details Go Here -->
-							<a href="http://www.velov-lyon.nicolasduquesne.com" target="_blank">
-								<h2 class="text-uppercase">RésaVélo'V</h2>
-							</a>
+							<h2 class="text-uppercase">RésaVélo'V</h2>
 							<p class="item-intro text-muted">Application de réservation de vélo'v a Lyon</p>
-						<button class="btn btn-warning" data-dismiss="modal" type="button"><i class="fa fa-times"></i> Fermer</button>
+							<div class="row">
+								<a class="col-md-3 offset-md-2 btn btn-success" href="http://www.velov-lyon.nicolasduquesne.com" target="_blank">Voir le site</a>
+								<button class="col-md-3 offset-md-2 btn btn-warning" data-dismiss="modal" type="button"><i class="fa fa-times"></i> Fermer</button>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -162,11 +168,12 @@
 					<div class="col-lg-8 mx-auto">
 						<div class="modal-body">
 						<!-- Project Details Go Here -->
-							<a href="http://www.nicolasduquesne.com/public/game/snake.html" target="_blank">
-								<h2 class="text-uppercase">Le Snake</h2>
-							</a>
+							<h2 class="text-uppercase">Le Snake</h2>
 							<p class="item-intro text-muted">Le célèbre jeux du serpent</p>
-						<button class="btn btn-warning" data-dismiss="modal" type="button"><i class="fa fa-times"></i> Fermer</button>
+							<div class="row">
+								<a class="col-md-3 offset-md-2 btn btn-success" href=http://www.nicolasduquesne.com/public/game/snake.html target="_blank">Voir le site</a>
+								<button class="col-md-3 offset-md-2 btn btn-warning" data-dismiss="modal" type="button"><i class="fa fa-times"></i> Fermer</button>
+							</div>
 						</div>
 					</div>
 				</div>
@@ -187,11 +194,12 @@
 					<div class="col-lg-8 mx-auto">
 						<div class="modal-body">
 						<!-- Project Details Go Here -->
-							<a href="http://www.blog-ecrivain.nicolasduquesne.com" target="_blank">
-								<h2 class="text-uppercase">Billet Simple Pour L'Alaska</h2>
-							</a>
+							<h2 class="text-uppercase">Billet Simple Pour L'Alaska</h2>
 							<p class="item-intro text-muted">Blog de Jean Forteroche</p>
-						<button class="btn btn-warning" data-dismiss="modal" type="button"><i class="fa fa-times"></i> Fermer</button>
+							<div class="row">
+								<a class="col-md-3 offset-md-2 btn btn-success" href="http://www.blog-ecrivain.nicolasduquesne.com" target="_blank">Voir le site</a>
+								<button class="col-md-3 offset-md-2 btn btn-warning" data-dismiss="modal" type="button"><i class="fa fa-times"></i> Fermer</button>
+							</div>
 						</div>
 					</div>
 				</div>
