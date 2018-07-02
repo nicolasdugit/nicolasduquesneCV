b650ecdbba499559bf9c5a158e0651b6a1565764
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-07-02 14:00:18 +0700

    traduction en anglais PROGRESSSection

--- view/en/includes/progressSection.php
+++ view/en/includes/progressSection.php
@@ -5,7 +5,7 @@
 				<div class="radial-prog-area margin-b-30">
 					<div class="radial-progress" data-prog-percent=".90">
 						<div></div>
-						<h6 class="progress-title">DEVELOPPEMENT WEB</h6>
+						<h6 class="progress-title">WEB DEVELOPMENT</h6>
 					</div>
 				</div><!-- radial-prog-area-->
 			</div><!-- col-sm-6-->
@@ -32,7 +32,7 @@
 				<div class="radial-prog-area margin-b-50">
 					<div class="radial-progress" data-prog-percent=".80">
 						<div></div>
-						<h6 class="progress-title">REFERENCEMENT</h6>
+						<h6 class="progress-title">SEO</h6>
 					</div>
 				</div><!-- radial-prog-area-->
 			</div><!-- col-sm-6-->
