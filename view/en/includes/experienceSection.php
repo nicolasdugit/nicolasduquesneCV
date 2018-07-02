3b84e79546f8c64ecf7aaf2fe04c0bf5500ae373
Nicolas Duquesne <duquesne.nico@gmail.com>
2018-07-02 13:57:16 +0700

    traduction en anglais experienceSection

--- view/en/includes/experienceSection.php
+++ view/en/includes/experienceSection.php
@@ -3,20 +3,19 @@
 		<div class="row">
 			<div class="col-sm-12">
 				<div class="heading">
-					<h3><b>Expériences Profesionnelles</b></h3>
+					<h3><b>Work Experiences</b></h3>
 					<h6 class="font-lite-black"><b></b></h6>
 				</div>
 			</div>
 			<div class="col-sm-12">
 				<div class="experience margin-b-50">
-					<h4><b>DEVELOPPEUR WEB INDEPENDANT</b></h4>
+					<h4><b>WEB DEVELOPER</b></h4>
 					<h5 class="font-yellow"><b>UPSTAIRSLAB</b></h5>
-					<h6 class="font-lite-black margin-t-10">SEPTEMBRE 2017 - AUJOURD'HUI</h6>
-					<p class="margin-tb-30"> Depuis septembre 2017, je travaille comme developpeur independant au sein de l'équipe <a href="http://www.upstairslab.nicolasduquesne">d'UpstairsLab</a></p>
+					<h6 class="font-lite-black margin-t-10">SEPTEMBER 2017 - PRESENT</h6>
+					<p class="margin-tb-30"> Since September 2017, I work as a freelance developer within the <a href="http://www.upstairslab.nicolasduquesne">UpstairsLab</a> team.</p>
 					<ul class="list margin-b-30">
-						<li><span>Développement de site web</span></li>
-						<li><span>Organisation de Vientiane Tech Talks : conférences autour des nouvelles technologies</span></li>
-						<li><span>Formation diplomante sur <a href="http://www.openclassrooms.fr">Openclassrooms</a></span></li>
+						<li><span>Web Development</span></li>
+						<li><span>Organization of Vientiane Tech Talks : conferences about new technologies</span></li>
 					</ul>
 				</div>
 			</div>
